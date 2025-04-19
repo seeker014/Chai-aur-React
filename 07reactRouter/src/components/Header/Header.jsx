4d62db35d8dbf2defd6a8bb8fe58c95def179aaa
seeker014 <abhinav.jee22@gmail.com>
2025-04-19 16:40:06 +0530

    router in react , useLoaderData hook

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="#"    // # means link goes nowhere right now
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
@@ -36,7 +36,7 @@ export default function Header() {
                                 <NavLink
                                 to="/"
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-orange-700' : 'text-gray-700'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
