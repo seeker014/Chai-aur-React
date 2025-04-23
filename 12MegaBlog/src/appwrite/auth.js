dc3692c1dc2ae8fd9c26d0b128e391e575034e27
seeker014 <abhinav.jee22@gmail.com>
2025-04-23 13:02:05 +0530

    auth file (read doc in appwrite) , client , account , login function

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import conf from '../conf.js'
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {            // whenever new object is made , constructor is called
+        this.client
+            .setEndpoint(conf.appwriteUrl)    // getting this syntax from doc of appwrite
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});   // if user account exists , go to login
+            } else {
+               return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;  // if we dont get into try catch block (just safety)
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();  // all functioons present in doc
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService  // exporting the object created in above line
+
+
